3f6202382aba58dc0dcbce5cecc2c5331cf43034
Siar Wahidi <siar.wahidi@gmail.com>
2022-02-15 20:28:50 -0500

    Registered for api key and stored it into variable. Made the comments section populate without hard coded array but it's only pulling the first comment. Will need to look into this

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,20 +1,71 @@
-const commentArray = [
-    {
-        name: "Connor Walton",
-        time: "02/17/2021",
-        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Letus appreciate this for what it is and what it contains."
-    },
-    {
-        name: "Emilie Beach",
-        time: "01/09/2021",
-        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-    },
-    {
-        name: "Miles Acosta",
-        time: "12/20/2020",
-        comment: "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough."
-    }
-];
+const api_key = '44207bb3-693f-4332-bf44-81855a3f337b';
+
+// const commentArray = [
+//     {
+//         name: "Connor Walton",
+//         time: "02/17/2021",
+//         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Letus appreciate this for what it is and what it contains."
+//     },
+//     {
+//         name: "Emilie Beach",
+//         time: "01/09/2021",
+//         comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+//     },
+//     {
+//         name: "Miles Acosta",
+//         time: "12/20/2020",
+//         comment: "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough."
+//     }
+// ];
+
+
+
+
+axios.get('https://project-1-api.herokuapp.com/comments/?api_key=44207bb3-693f-4332-bf44-81855a3f337b')
+.then(response => {
+    // console.log(response.data);
+
+            for(let i = 0; i < response.data.length; i++) {
+                // console.log(response.data[i]);
+
+            // this is the main container
+            const divContainer = document.createElement('div');
+            divContainer.classList.add('comments-added__section');
+            comments.appendChild(divContainer);
+
+            //trying div for name and date
+            const flexDiv = document.createElement('div');
+            flexDiv.classList.add('comments__div');
+            divContainer.appendChild(flexDiv);
+
+            // image of avatar
+            const image = document.createElement('img');
+            image.classList.add('comments-added__avatar');
+            flexDiv.appendChild(image);
+
+            // name
+            const name = document.createElement('p');
+            name.innerText = response.data[0].name;
+            name.classList.add('comments-added__name');
+            flexDiv.appendChild(name);
+
+            // date/time
+            const time = document.createElement('p');
+            time.innerText = response.data[0].timestamp;
+            time.classList.add('comments-added__date');
+            flexDiv.appendChild(time);
+
+            // comment body
+            const actualComment = document.createElement('p');
+            actualComment.innerText = response.data[0].comment;
+            actualComment.classList.add('comments-added__body');
+            divContainer.appendChild(actualComment);
+
+
+
+            }
+})
+
 
 const comments = document.querySelector(".comments-added");
 
@@ -59,8 +110,8 @@ const comments = document.querySelector(".comments-added");
     
         }
     }
-
-        displayComment();
+    // ------commented out function so doesnt run-----
+        // displayComment();
 
         // shorten date
         const date = new Date();
