f8457de88e481407655923439b87547e1438b20e
Siar Wahidi <siar.wahidi@gmail.com>
2022-02-18 20:41:06 -0500

    created const variables for url and api key and used those variable within axios. Removed all old commented out code from previous sprint. Created function for new post and called it within axios

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,189 +1,7 @@
-api_key = '44207bb3-693f-4332-bf44-81855a3f337b';
-
-//  const showsArray = [{
-//     date: "Mon Sept 06 2021",
-//     venue: "Ronald Lane",
-//     location: "San Francisco, CA"
-// },
-// {
-//     date: "Tue Sept 21 2021",
-//     venue: "Pier 3 East",
-//     location: "San Francisco, CA"
-// },
-// {
-//     date: "Fri Oct 15 2021",
-//     venue: "View Lounge",
-//     location: "San Francisco, CA"
-// },
-// {
-//     date: "Sat Nov 06 2021",
-//     venue: "Hyatt Agency",
-//     location: "San Francisco, CA"
-// },
-// {
-//     date: "Fri Nov 26 2021",
-//     venue: "Moscow Center",
-//     location: "San Francisco, CA"
-// },
-// {
-//     date: "Wed Dec 15 2021",
-//     venue: "Press Club",
-//     location: "San Francisco, CA"
-// }
-// ]
-
-// function displayShows(array) {
-
-// if(showsArray.length === 0){
-//     return;
-// }
-
-// let counter = 0;
-// const showsWrapper = document.querySelector('.shows-wrapper');
-
-// for (const key in array) {
-
-//     if (counter !== 0) {
-//             const shows = document.createElement('section');
-//             shows.classList.add('shows');
-//             showsWrapper.appendChild(shows);
-
-//             // shows container
-//             const showsContainer = document.createElement('div');
-//             showsContainer.classList.add('shows__container');
-//             shows.appendChild(showsContainer);
-
-//             // ul
-//             const showsUlOne = document.createElement('ul');
-//             showsUlOne.classList.add('shows__list');
-//             showsContainer.appendChild(showsUlOne);
-
-//             // li header
-//             const showsLi = document.createElement('li');
-//             showsLi.classList.add('shows__list__date', 'shows__list__date--hidden');
-//             showsLi.innerText = 'DATE';
-//             showsUlOne.appendChild(showsLi);
-
-//             // li from JS for Date
-//             const showsListDate = document.createElement('li');
-//             showsListDate.classList.add('shows__list__date__text');
-//             showsListDate.innerText = array[key].date;
-//             showsUlOne.appendChild(showsListDate);
-
-//             const showsUlTwo = document.createElement('ul');
-//             showsUlTwo.classList.add('shows__list');
-//             showsContainer.appendChild(showsUlTwo);
-
-//             // li venue header
-//             const showsVenue = document.createElement('li');
-//             showsVenue.classList.add('shows__list__venue', 'shows__list__venue--hidden');
-//             showsVenue.innerText = 'VENUE';
-//             showsUlTwo.appendChild(showsVenue);
-
-//             // li from JS for venue
-//             const showsVenueList = document.createElement('li');
-//             showsVenueList.classList.add('shows__list__venue__text');
-//             showsVenueList.innerText = array[key].venue;
-//             showsUlTwo.appendChild(showsVenueList);
-
-//             const showsUlThree = document.createElement('ul');
-//             showsUlThree.classList.add('shows__list');
-//             showsContainer.appendChild(showsUlThree);
-
-//             // li location header
-//             const showsLocation = document.createElement('li');
-//             showsLocation.classList.add('shows__list__location', 'shows__list__location--hidden');
-//             showsLocation.innerText = 'LOCATION';
-//             showsUlThree.appendChild(showsLocation);
-
-//             // li from JS for location
-//             const showsLocationList = document.createElement('li');
-//             showsLocationList.classList.add('shows__list__location__text');
-//             showsLocationList.innerText = array[key].location;
-//             showsUlThree.appendChild(showsLocationList);
-
-//             // btn
-//             const btn = document.createElement('button');
-//             btn.classList.add('btn');
-//             btn.innerText = 'Buy Tickets';
-//             showsContainer.appendChild(btn);
-
-//         } else {
-
-//             // section
-//             const shows = document.createElement('section');
-//             shows.classList.add('shows');
-//             showsWrapper.appendChild(shows);
-
-//             //container
-//             const showsContainer = document.createElement('div');
-//             showsContainer.classList.add('shows__container');
-//             shows.appendChild(showsContainer)
-
-//             // =========== DATE======================
-//             //static one -> date
-//             const showsListStaticOne = document.createElement('ul');
-//             showsListStaticOne.classList.add('shows__list');
-//             showsContainer.appendChild(showsListStaticOne);
-
-//             const showsLi = document.createElement('li');
-//             showsLi.classList.add('shows__list__date');
-//             showsLi.innerText = 'DATE';
-//             showsListStaticOne.appendChild(showsLi);
-
-//             // ==========VENUE=================
-
-//             const showsListStaticTwo = document.createElement('ul');
-//             showsListStaticTwo.classList.add('shows__list');
-//             showsContainer.appendChild(showsListStaticTwo);
-
-//             const showsVenue = document.createElement('li');
-//             showsVenue.classList.add('shows__list__venue');
-//             showsVenue.innerText = 'VENUE';
-//             showsListStaticTwo.appendChild(showsVenue);
-
-//             //==============Location ==================
-//             const showsListStaticThree = document.createElement('ul');
-//             showsListStaticThree.classList.add('shows__list');
-//             showsContainer.appendChild(showsListStaticThree);
-
-//             // li location header
-//             const showsLocation = document.createElement('li');
-//             showsLocation.classList.add('shows__list__location');
-//             showsLocation.innerText = 'LOCATION';
-//             showsListStaticThree.appendChild(showsLocation);
-
-//             const btn = document.createElement('button');
-//             btn.classList.add('btn');
-//             btn.innerText = 'Buy Tickets';
-//             showsContainer.appendChild(btn);
-
-//             const showsListDate = document.createElement('li');
-//             showsListDate.classList.add('shows__list__date__text');
-//             showsListDate.innerText = array[key].date;
-
-//             const showsVenueList = document.createElement('li');
-//             showsVenueList.classList.add('shows__list__venue__text');
-//             showsVenueList.innerText = array[key].venue;
-
-//             const showsLocationList = document.createElement('li');
-//             showsLocationList.classList.add('shows__list__location__text');
-//             showsLocationList.innerText = array[key].location
+const apiKey = '?api_key=44207bb3-693f-4332-bf44-81855a3f337b';
+const showsUrl = 'https://project-1-api.herokuapp.com/showdates/';
 
 
-//             showsListStaticOne.appendChild(showsListDate);
-//             showsListStaticTwo.appendChild(showsVenueList);
-//             showsListStaticThree.appendChild(showsLocationList);
-//         }
-//     counter++;
-//     }
-// }
-
-// displayShows(showsArray);
-
-
-
-// Re-wrote function to pull data from db
 function displayShows(response) {
 
     if(response.data.length === 0){
@@ -219,7 +37,6 @@ function displayShows(response) {
                 // li from JS for Date
                 const showsListDate = document.createElement('li');
                 showsListDate.classList.add('shows__list__date__text');
-                // showsListDate.innerText = array[key].date;
                 showsListDate.innerText = new Date(Number(response.data[key].date)).toDateString();
               
                 showsUlOne.appendChild(showsListDate);
@@ -237,7 +54,6 @@ function displayShows(response) {
                 // li from JS for venue
                 const showsVenueList = document.createElement('li');
                 showsVenueList.classList.add('shows__list__venue__text');
-                // showsVenueList.innerText = array[key].venue;
                 showsVenueList.innerText = response.data[key].place;
                 showsUlTwo.appendChild(showsVenueList);
     
@@ -254,7 +70,6 @@ function displayShows(response) {
                 // li from JS for location
                 const showsLocationList = document.createElement('li');
                 showsLocationList.classList.add('shows__list__location__text');
-                // showsLocationList.innerText = array[key].location;
                 showsLocationList.innerText = response.data[key].location;
                 showsUlThree.appendChild(showsLocationList);
     
@@ -316,20 +131,17 @@ function displayShows(response) {
     
                 const showsListDate = document.createElement('li');
                 showsListDate.classList.add('shows__list__date__text');
-                // showsListDate.innerText = array[key].date;
                 showsListDate.innerText = new Date(Number(response.data[key].date)).toDateString();
     
                 const showsVenueList = document.createElement('li');
                 showsVenueList.classList.add('shows__list__venue__text');
-                // showsVenueList.innerText = array[key].venue;
                 showsVenueList.innerText = response.data[key].place;
     
                 const showsLocationList = document.createElement('li');
                 showsLocationList.classList.add('shows__list__location__text');
-                // showsLocationList.innerText = array[key].location
                 showsLocationList.innerText = response.data[key].location
     
-    
+                // appends
                 showsListStaticOne.appendChild(showsListDate);
                 showsListStaticTwo.appendChild(showsVenueList);
                 showsListStaticThree.appendChild(showsLocationList);
@@ -337,9 +149,10 @@ function displayShows(response) {
         counter++;
         }
     }
-
-axios.get('https://project-1-api.herokuapp.com/showdates/?api_key=44207bb3-693f-4332-bf44-81855a3f337b')
+axios.get(showsUrl+apiKey)
 .then(response => {
-    console.log(response);
     displayShows(response);
+})
+.catch(err => {
+    console.log('Sorry, unable to retrieve data');
 })
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,75 +1,15 @@
-const api_key = '44207bb3-693f-4332-bf44-81855a3f337b';
+const apiKey = '?api_key=44207bb3-693f-4332-bf44-81855a3f337b';
+const commentsUrl = 'https://project-1-api.herokuapp.com/comments/';
 
-// const commentArray = [
-//     {
-//         name: "Connor Walton",
-//         time: "02/17/2021",
-//         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Letus appreciate this for what it is and what it contains."
-//     },
-//     {
-//         name: "Emilie Beach",
-//         time: "01/09/2021",
-//         comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-//     },
-//     {
-//         name: "Miles Acosta",
-//         time: "12/20/2020",
-//         comment: "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough."
-//     }
-// ];
-
-
-
-
-axios.get('https://project-1-api.herokuapp.com/comments/?api_key=44207bb3-693f-4332-bf44-81855a3f337b')
-.then(response => {
-    displayComment(response)
-    // console.log(response.data);
-
-            // for(let i = 0; i < response.data.length; i++) {
-                // console.log(response.data[i]);
-
-            // this is the main container
-            // const divContainer = document.createElement('div');
-            // divContainer.classList.add('comments-added__section');
-            // comments.appendChild(divContainer);
-
-            //trying div for name and date
-            // const flexDiv = document.createElement('div');
-            // flexDiv.classList.add('comments__div');
-            // divContainer.appendChild(flexDiv);
-
-            // image of avatar
-            // const image = document.createElement('img');
-            // image.classList.add('comments-added__avatar');
-            // flexDiv.appendChild(image);
-
-            // name
-            // const name = document.createElement('p');
-            // name.innerText = response.data[i].name;
-            // name.classList.add('comments-added__name');
-            // flexDiv.appendChild(name);
-
-            // date/time
-            // const time = document.createElement('p');
-            // time.innerText = response.data[i].timestamp;
-            // time.innerText = new Date(response.data[i].timestamp).toLocaleDateString();
-            // time.classList.add('comments-added__date');
-            // flexDiv.appendChild(time);
-
-            // comment body
-            // const actualComment = document.createElement('p');
-            // actualComment.innerText = response.data[i].comment;
-            // actualComment.classList.add('comments-added__body');
-            // divContainer.appendChild(actualComment);
-            // }
-}).catch(err => {
-    console.log("Sorry, we weren't able to retrieve comments...");
+axios.get(commentsUrl+apiKey)
+    .then(response => {
+     displayComment(response)
+    })
+    .catch(err => {
+    console.log('Sorry, unable to retrieve data');
 })
 
 
-
-// trying to store comment populating into function instead
 function displayComment(response) {
 
         // sorting response
@@ -81,7 +21,7 @@ function displayComment(response) {
         const comments = document.querySelector(".comments-added");
         comments.innerText = '';
 
-    for(let i = 0; i < response.data.length; i++) {
+        for(let i = 0; i < response.data.length; i++) {
 
         // this is the main container
         const divContainer = document.createElement('div');
@@ -115,240 +55,55 @@ function displayComment(response) {
         actualComment.innerText = response.data[i].comment;
         actualComment.classList.add('comments-added__body');
         divContainer.appendChild(actualComment);
-
+    }
 }
-}
-
-// ---- this is now being pulled from database ------
-    // function displayComment() {
-
-    //     for(const key in commentArray) {
-        
-    //     // this is the main container
-    //     const divContainer = document.createElement('div');
-    //     divContainer.classList.add('comments-added__section');
-    //     comments.appendChild(divContainer);
-
-    //     //trying div for name and date
-    //     const flexDiv = document.createElement('div');
-    //     flexDiv.classList.add('comments__div');
-    //     divContainer.appendChild(flexDiv);
-
-    //     // image of avatar
-    //     const image = document.createElement('img');
-    //     image.classList.add('comments-added__avatar');
-    //     flexDiv.appendChild(image);
-  
-    //    // name
-    //     const name = document.createElement('p');
-    //     name.innerText = commentArray[key].name;
-    //     name.classList.add('comments-added__name');
-    //     flexDiv.appendChild(name);
-    
-    //     // date/time
-    //     const time = document.createElement('p');
-    //     time.innerText = commentArray[key].time;
-    //     time.classList.add('comments-added__date');
-    //     flexDiv.appendChild(time);
-
-    //     // comment body
-    //     const actualComment = document.createElement('p');
-    //     actualComment.innerText = commentArray[key].comment;
-    //     actualComment.classList.add('comments-added__body');
-    //     divContainer.appendChild(actualComment);
-    
-    //     comments.appendChild(divContainer);
-    
-    //     }
-    // }
-  
-    //     displayComment();
-
-        // shorten date
-        // const date = new Date();
-        // const newDate = date.toLocaleDateString('en-US');
-        const mainForm = document.getElementById('mainForm');
-
-        // -----------Below will need to be changed to POST method-----
-        ///// form submit
-        // mainForm.addEventListener('submit', (e) => {
-        // e.preventDefault();
-
-        // const formName = e.target.name.value;
-        // const formComment = e.target.comment.value;
-        // const formTime = newDate;
-        // const comments = document.querySelector(".comments-added");
-
-        // if(formName && formComment !== "") {
-    
-            // this is the main container
-            // const divContainer = document.createElement('div');
-            // divContainer.classList.add('comments-added__section');
-            // comments.appendChild(divContainer);
-
-            // trying div for name and date
-            // const flexDiv = document.createElement('div');
-            // flexDiv.classList.add('comments__div');
-            // divContainer.appendChild(flexDiv);
-
-            // image of avatar
-            // const image = document.createElement('img');
-            // image.classList.add('comments-added__avatar');
-            // flexDiv.appendChild(image);
-
-            // name
-            // const name = document.createElement('p');
-            // name.innerText = formName;
-            // name.classList.add('comments-added__name');
-            // flexDiv.appendChild(name);
-
-
-            // date/time
-            // const time = document.createElement('p');
-            // time.innerText = formTime;
-            // time.classList.add('comments-added__date');
-            // flexDiv.appendChild(time);
-
-            // divContainer.appendChild(flexDiv);
-
- 
-            // comment body
-//             const actualComment = document.createElement('p');
-//             actualComment.innerText = formComment;
-//             actualComment.classList.add('comments-added__body');
-//             divContainer.appendChild(actualComment);
-    
-    
-//             comments.appendChild(divContainer);
-//             comments.insertBefore(divContainer, comments.childNodes[0]);
-   
-//     }       
-// formValidation(e);
-
-// });
 
 // simple form validation
-// function formValidation(e) {
-//     const nameValidation = document.getElementById('nameValidate');
-//     const commentValidation = document.getElementById('commentValidate');
-//     if(nameValidation.value === '') {
-//         nameValidation.style.borderColor = '#d22d2d';
-//     }
-//     if(commentValidation.value === '') {
-//         commentValidation.style.borderColor = '#d22d2d';
-//     } 
-//     else {
-//         e.target.name.value = '';
-//         e.target.comment.value = '';
-//     }
-// }
+function formValidation(e) {
+    const nameValidation = document.getElementById('nameValidate');
+    const commentValidation = document.getElementById('commentValidate');
+    if(nameValidation.value === '') {
+        nameValidation.style.borderColor = '#d22d2d'; 
+    }
+    if(commentValidation.value === '') {
+        commentValidation.style.borderColor = '#d22d2d';  
+    } 
+    return false;
+}
 
+function formPost(name, comment) {
+    let newPost = {
+        name: name,
+        comment: comment
+    }
 
-
-// ------ Below is posting new comments using POST method-----
-
-
-
-
-
-// ----- below posted to database but form didnt update--------
-//   axios({
-//       method: "post",
-//      url: "https://project-1-api.herokuapp.com/comments/?api_key=44207bb3-693f-4332-bf44-81855a3f337b",
-//      data: {
-//          name: e.target.name.value,
-//          comment: e.target.comment.value,
-//      }
-//   }).then((response) => {
-//       console.log(response)
-//   });
-//     mainForm.addEventListener('submit', (e) => {
-//         e.preventDefault();
-    
-//         formName = e.target.name.value;
-//         formComment = e.target.comment.value;
-//     });
-//  })
-
-// --------------Trying to add comments form straight to db-------------
-
-// mainForm.addEventListener('submit', (e) => {
-//     e.preventDefault();
-
-//     axios.post('https://project-1-api.herokuapp.com/comments/?api_key=44207bb3-693f-4332-bf44-81855a3f337b', 
-//     {
-//         name: e.target.name.value,
-//         comment: e.target.comment.value
-//     }).then(response => {
-        // this gives me an object of what I just POST
-        // console.log(response);
-
-        // displayComment(response);
-
-        //const comments = document.querySelector(".comments-added");
-
-            //  this is the main container
-            //   const divContainer = document.createElement('div');
-            //   divContainer.classList.add('comments-added__section');
-            //   comments.appendChild(divContainer);
-
-            // trying div for name and date
-            //   const flexDiv = document.createElement('div');
-            //   flexDiv.classList.add('comments__div');
-            //   divContainer.appendChild(flexDiv);
-
-             // image of avatar
-            //   const image = document.createElement('img');
-            //   image.classList.add('comments-added__avatar');
-            //   flexDiv.appendChild(image);
-
-            // name
-            //   const name = document.createElement('p');
-            //   name.innerText = e.target.name.value;
-            //   name.classList.add('comments-added__name');
-            //   flexDiv.appendChild(name);
-
-            // date/time
-            //   const time = document.createElement('p');
-            //   time.innerText = response.data.timestamp;
-            //   time.classList.add('comments-added__date');
-            //   flexDiv.appendChild(time);
-
-            //   divContainer.appendChild(flexDiv);
-
-            // comment body
-            //   const actualComment = document.createElement('p');
-            //   actualComment.innerText = e.target.comment.value;
-            //   actualComment.classList.add('comments-added__body');
-            //   divContainer.appendChild(actualComment);
-
-            //   comments.appendChild(divContainer);
-            //   comments.insertBefore(divContainer, comments.childNodes[0]);
-
-//     })
-
-
-// })
-
-
-mainForm.addEventListener("submit", (e) => {
-    e.preventDefault()
-
-    axios.post('https://project-1-api.herokuapp.com/comments/?api_key=44207bb3-693f-4332-bf44-81855a3f337b',
-    {
-        name: e.target.name.value,
-        comment: e.target.comment.value
-    })
-    .then(() => {
-        axios.get('https://project-1-api.herokuapp.com/comments/?api_key=44207bb3-693f-4332-bf44-81855a3f337b')
+    axios.post(commentsUrl+apiKey, newPost)
+        .then(() => {
+        axios.get(commentsUrl+apiKey)
         .then(response => {
             displayComment(response)
         })
     })
+    .catch(err => {
+        console.log('Sorry, unable to retrieve data');
+    })
+}
 
-    // reset the form 
+const mainForm = document.getElementById('mainForm');
+mainForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    formValidation(e);
+
+    let name = e.target.name.value;
+    let comment = e.target.comment.value;
+
+    if(name && comment) {
+        formPost(name, comment)  
+    }
+    // reset the form    
     document.getElementById('mainForm').reset();
-});
-    
+})
+
+
 
     
